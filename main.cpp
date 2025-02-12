65891aef299cbcbd20453a12deef80c5ae506709
Dantetrevino <123134235+Dantetrevino@users.noreply.github.com>
2025-02-12 13:22:01 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  std::cout << "LAPTOP is botting up...\n";
+
+  if (authenticateUser()){
+    std::cout << "Successful. Welcome"
+    } else {
+  }
+
+  return 0;
+  }
