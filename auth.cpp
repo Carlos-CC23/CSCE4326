c8325d78cca22bafd71b2ce9ae8271b0e8437680
Carlos-CC23 <chaconcarlos17@yahoo.com>
2025-02-12 17:59:18 -0600

    Update auth.cpp

--- auth.cpp
+++ auth.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
+#include <string>
+#include "auth.h"
+using namespace std;
 
+bool authenticateUser(){
+  string userName, PWord;
+  const string userName = "ADMIN";
+  const string PWord = "OSisthebest23";
+
+  cout << "Enter username: " << userName << endl;
+  cout << "Enter password: " << PWord << endl;
+
+  if(userName == correctuserName && PWord == correctPWord){
+    cout << "This authetication is correct. Hello, " << userName << "!\n" << endl;
+    return true;
+  }
+  else{
+    cout << "This authetication is incorrect. Incorrect user OR password.\n" << endl
+      return false;
+  }
+  
+}
