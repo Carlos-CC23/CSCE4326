561e12c84fa8e975a3c6a981807384ab8f54b23b
Carlos-CC23 <chaconcarlos17@yahoo.com>
2025-02-12 22:59:56 -0600

    Update auth.cpp

--- auth.cpp
+++ auth.cpp
@@ -6,15 +6,19 @@ using namespace std;
 bool authenticateUser(){
   string userName; 
   string PWord;
+  
+  //The "hardcoded" authentication
   const string correctuserName = "ADMIN";
   const string correctPWord = "OSisthebest23";
-
+  
+  //Prompts user for logins
   cout << "Enter username: ";
   cin >> userName;
   cout << "Enter password: ";
   cin >> PWord;
   cout << "\n";
 
+  //Checks if the logins is correct or not
   if(userName == correctuserName && PWord == correctPWord){
     cout << "This authetication is correct. Hello, " << userName << "!\n" << endl;
     return true;
