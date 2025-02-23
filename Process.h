f054cdf84ddb0c4759fda630d097f7ca7baf51a3
Carlos-CC23 <chaconcarlos17@yahoo.com>
2025-02-23 01:59:54 -0600

    Update Process.h

--- Process.h
+++ Process.h
@@ -1,13 +1,12 @@
 #define PROCESS_H
 #ifndef PROCESS_H
-
 #include <iostream>
 #include <string>
 
 enum ProcessState {NEW, READY, RUNNING, WAITING, TERMINATED};
 
 //Convert the enum state into strings
-string StateToString{
+string stateToString{
     switch(case){
         case NEW:
             return "NEW";
@@ -32,5 +31,46 @@ private:
   int waiting_time;
 
 public:
-  Process(int pid)
-  : pid(pid){}
+  Process(int pid, int arrival_time, int burst_time, int priority, int memory_required, bool io_operations)
+  : pid(pid), arrival_time(arrival_time), burst_time(burst_time), 
+    priority(priority), state(NEW), remaining_time(burst_time), 
+    waiting_time(0), turnaround_time(0), memory_required(memory_required),
+    io_operations(io_operations)
+{}
+
+//Getters for the process attributes
+int getPid() const{
+    return pid;
+}
+int getArrivalTime() const {
+    return ArrivalTime;
+}
+int getBurstTime() const {
+    return burst_time;
+}
+int getPriority() const {
+    return priority;
+}
+ProcessState getState() const {
+    return state;
+}
+int getRemainingTime() const {
+    return remaining_time;
+}
+int getWaitingTime() const {
+    return waiting_time;
+}
+int getTurnaroundTime() const {
+    return turnaround_time;
+}
+int getMemoryRequired() const {
+    return memory_required;
+}
+bool hasIOOperations() const {
+    return io_operations;
+}
+
+
+
+};
+
