ba5765aa90bddce75a93ff3f0b07deba021d7745
Carlos-CC23 <chaconcarlos17@yahoo.com>
2025-02-22 22:03:03 -0600

    Update Process.h

--- Process.h
+++ Process.h
@@ -4,6 +4,26 @@
 #include <iostream>
 #include <string>
 
+enum ProcessState {NEW, READY, RUNNING, WAITING, TERMINATED};
+
+//Convert the enum state into strings
+string StateToString{
+    switch(case){
+        case NEW:
+            return "NEW";
+        case READY;
+            return "READY";
+        case RUNNNING:
+            return "RUNNING";
+        case WAITING:
+            return "WAITING";
+        case TERMINATED:
+            return "TERMINATED";
+        default:
+            return "UNKNOWN";
+    }
+}
+
 class Process {
 private:
   int pid;
