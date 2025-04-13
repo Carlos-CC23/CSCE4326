b3fcec7c4489c28533cd6f8c68e718cd1065b2ed
Dantetrevino <123134235+Dantetrevino@users.noreply.github.com>
2025-04-13 18:03:26 -0500

    Update Process.h

--- Process.h
+++ Process.h
@@ -1,8 +1,20 @@
 #ifndef PROCESS_H
 #define PROCESS_H
+#define PAGE_SIZE 4096 // 4KB
+#define VIRTUAL_MEMORY_SIZE (64 * 1024) // 64KB per process
+#define PHYSICAL_MEMORY_SIZE (32 * 1024) // 32KB total
+#define NUM_VIRTUAL_PAGES (VIRTUAL_MEMORY_SIZE / PAGE_SIZE)
+#define NUM_PHYSICAL_FRAMES (PHYSICAL_MEMORY_SIZE / PAGE_SIZE)
 
 #include <iostream>
+#include <vector>
 #include <string>
+
+struct PageTableEntry {
+    bool valid;
+    int frameNumber;
+    bool dirty;
+};
 
 enum ProcessState {NEW, READY, RUNNING, WAITING, TERMINATED};
 
@@ -19,13 +31,14 @@ private:
     int memory_required; //Memory required by the process
     bool io_operations; // Indicates if the process has I/O Operations
     ProcessState state; //The current process states -> NEW, READY, RUNNING, WAITING, TERMINATED
+    std::vector<PageTableEntry> pageTable;
 
 
 public:
     //constructor
     Process(int pid, int arrival_time, int burst_time, int priority, int memory_required, bool io_operations);
 
-//getters
+    //getters
     int getPid() const;
     int getArrivalTime() const;
     int getBurstTime() const;
@@ -49,6 +62,11 @@ public:
     void runProcess(int timeSlice); //simulate running the process
     std::string getStateString() const;
     void displayProcessInfo() const; //improved display
-};
 
+    //Virtual address
+    void initializePageTable();
+    int translateVirtualAddress(int virtualAddress) const;
+    void mapPage(int virtualPage, int frameNumber); // optional helper
+
+};
 #endif // PROCESS_H
